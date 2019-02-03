4d9109b8680d6cd253dce26540c7fc77caa5c81e
Melissa Hummelt <mkhummelt@gmail.com>
2019-02-03 17:55:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,15 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(names)# code an argument here
   # Your code here
+  names.collect
+  list = []
+    i = 0
+    while i < names.length
+      list << yield(names[i])
+      i += 1
+    end
+    list
+
+
 end
 
 def summon_captain_planet# code an argument here
