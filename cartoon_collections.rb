2183631336a5ae33f742a6bb2c680ce95c187def
Melissa Hummelt <mkhummelt@gmail.com>
2019-02-03 18:02:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(name)# code an argument here
   # Your code here
-  names.each_with_index do |name, index|
+  name.each_with_index do |name, index|
     puts "#{index}. #{name}"
   end
 end
