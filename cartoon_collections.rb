4a39fa0e61f459fb8ff7eb22579c338517d1cf24
Melissa Hummelt <mkhummelt@gmail.com>
2019-02-03 17:59:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ def roll_call_dwarves(names)# code an argument here
   list = []
     i = 0
     while i < names.length
-      list << yield(names[i])
+      list << yield(names[i], index)
       i += 1
     end
     puts list
