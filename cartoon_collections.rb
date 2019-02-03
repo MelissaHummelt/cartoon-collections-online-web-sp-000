273ee5282d6a8899ce0d27a0b02014e4ed084b66
Melissa Hummelt <mkhummelt@gmail.com>
2019-02-03 17:56:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
-def roll_call_dwarves(names)# code an argument here
+def roll_call_dwarves(names, index)# code an argument here
   # Your code here
-  names.collect
+  names.each_with_index
   list = []
     i = 0
     while i < names.length
